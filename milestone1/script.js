73f9394a750db03f7f7a8973766fba56c7a598d6
Farwa Kanwal <164297525+Farwa-Kanwal-123@users.noreply.github.com>
2024-09-14 00:14:59 +0500

    Add files via upload
    push hackathone-milestone1

---
+++ milestone1/script.js
@@ -0,0 +1,13 @@
+//import id's from html file
+var toggleButton = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+//add an avent linstener for toggle button to hide or show skills section
+toggleButton.addEventListener('click', function () {
+    //condition for hide /show section when we click on b utton
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
